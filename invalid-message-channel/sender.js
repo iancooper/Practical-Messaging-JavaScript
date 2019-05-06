bff25ce50a87b184e0a1c28d5c084bbcebdeb2c9
Ian Cooper <ian.cooper@just-eat.com>
2019-05-06 15:33:17 -0500

    Fix issue with no serialization

--- invalid-message-channel/sender.js
+++ invalid-message-channel/sender.js
@@ -5,15 +5,14 @@ var greetingLib = require("./greetings.js");
 
 var done = false;
 
-var greeting = new greetingLib.Greetings("Hello World");
-
-const dataTypeChannel = new dataTypeLib.Producer("practical.messaging.invalid." + greeting.constructor.name, "amqp://guest:guest@localhost:5672", function(message){
+const dataTypeChannel = new dataTypeLib.Producer("practical.messaging.invalid.Greetings", "amqp://guest:guest@localhost:5672", function(message){
     return JSON.stringify(message);
 });
 
 console.log("Preparing to send message to consumers");
 
 dataTypeChannel.afterChannelOpened(function(channel){
+    var greeting = new greetingLib.Greetings("Hello World");
     dataTypeChannel.send(channel,"Hello World", function(){
         console.log("Message sent!");
         done = true;
